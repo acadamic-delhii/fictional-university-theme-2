4fb31a3027afba9bd2e338866e67ddfa8ddbcd73
Max <max@example.com>
2020-06-27 20:01:39 -0700

    WP_ext Lesson 016 Menu of Chile Page Links

--- page.php
+++ page.php
@@ -23,18 +23,34 @@ while (have_posts()) {
                 <p><a class="metabox__blog-home-link" href="<?php echo get_permalink($parentId); ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($parentId); ?></a>
                     <span class="metabox__main"><?php the_title(); ?></span></p>
             </div>
+        <?php
+        }
+
+        $childPages = get_pages(
+            array('child_of' => get_the_ID())
+        );
+
+        if ($parentId or $childPages) {
+        ?>
+            <div class="page-links">
+                <h2 class="page-links__title"><a href="<?php echo get_permalink($parentId)?>"><?php echo get_the_title($parentId); ?></a></h2>
+                <ul class="min-list">
+                    <?php
+                    if ($parentId) {
+                        $findChildrenOf = $parentId;
+                    } else {
+                        $findChildrenOf = get_the_ID();
+                    }
+                    wp_list_pages(array(
+                        'title_li' => NULL,
+                        'child_of' => $findChildrenOf,
+                        'sort_column' => 'menu_order'
+                    ));
+                    ?>
+                </ul>
+            </div>
+
         <?php } ?>
-
-        <!--
-        <div class="page-links">
-            <h2 class="page-links__title"><a href="#">About Us</a></h2>
-            <ul class="min-list">
-                <li class="current_page_item"><a href="#">Our History</a></li>
-                <li><a href="#">Our Goals</a></li>
-            </ul>
-        </div>
--->
-
         <div class="generic-content">
             <?php the_content(); ?>
         </div>
