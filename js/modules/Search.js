6c01dc39da7fd72982051ad5c786ba189db71bf3
Max <max@example.com>
2020-07-17 19:32:58 -0700

    WP_ext Lesson 053 Open and Close Search Overlay

--- js/modules/Search.js
+++ js/modules/Search.js
@@ -1,6 +1,22 @@
 class Search {
     constructor() {
-        alert("test1");
+        this.openButton = document.querySelector(".js-search-trigger");
+        this.closeButton = document.querySelector(".search-overlay__close");
+        this.searchOverlay = document.querySelector(".search-overlay");
+        this.event();
+    }
+
+    event() {
+        this.openButton.addEventListener("click", () => this.openOverlay());
+        this.closeButton.addEventListener("click", () => this.closeOverlay());
+    }
+
+    openOverlay() {
+        this.searchOverlay.classList.add("search-overlay--active");
+    }
+
+    closeOverlay() {
+        this.searchOverlay.classList.remove("search-overlay--active");
     }
 }
 
