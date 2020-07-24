b6a2e5b3d605fde2dc8ee78b154dcf3d0a256bc9
Max <max@example.com>
2020-07-24 07:43:24 -0700

    WP_ext Lesson 069 Working With Multiple Post Types

--- inc/search-route.php
+++ inc/search-route.php
@@ -2,7 +2,7 @@
 
 add_action('rest_api_init', 'universityRegisterSearch');
 
-function universityRegisterSearch ()
+function universityRegisterSearch()
 {
     register_rest_route('university/v1', 'search', array(
         'methods' => WP_REST_SERVER::READABLE,
@@ -10,22 +10,60 @@ function universityRegisterSearch ()
     ));
 }
 
-function universitySearchResults($data) {
-    $professor = new WP_Query(array(
-        'post_type' => 'professor', 
+function universitySearchResults($data)
+{
+    $mainQuery = new WP_Query(array(
+        'post_type' => array('post', 'page', 'professor', 'program', 'campus', 'event'),
         's' => sanitize_text_field($data['term'])
     ));
 
-    $professorResults = array();
+    $results = array(
+        'generalInfo' => array(),
+        'professors' => array(),
+        'programs'   => array(),
+        'events'     => array(),
+        'campuses'   => array()
+    );
 
-    while($professor->have_posts()) {
-        $professor->the_post();
+    while ($mainQuery->have_posts()) {
+        $mainQuery->the_post();
 
-        array_push($professorResults, array(
-            'title' => get_the_title(),
-            'permalink' => get_the_permalink()
-        ));
+        switch (get_post_type()) {
+            case 'post':
+            case 'page':
+                array_push($results['generalInfo'], array(
+                    'title' => get_the_title(),
+                    'permalink' => get_the_permalink()
+                ));
+                break;
+            case 'professor':
+                array_push($results['professors'], array(
+                    'title' => get_the_title(),
+                    'permalink' => get_the_permalink()
+                ));
+                break;
+            case 'program':
+                array_push($results['programs'], array(
+                    'title' => get_the_title(),
+                    'permalink' => get_the_permalink()
+                ));
+                break;
+            case 'event':
+                array_push($results['events'], array(
+                    'title' => get_the_title(),
+                    'permalink' => get_the_permalink()
+                ));
+                break;
+            case 'campus':
+                array_push($results['campuses'], array(
+                    'title' => get_the_title(),
+                    'permalink' => get_the_permalink()
+                ));
+                break;
+            default:
+                break;
+        }
     }
 
-    return $professorResults;
+    return $results;
 }
