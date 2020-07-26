0dde9ad2858783ce411a9f20cbf8c3e20408cf9d
Max <max@example.com>
2020-07-26 07:05:18 -0700

    WP_ext Lesson 050 Final Campus Details

--- functions.php
+++ functions.php
@@ -134,6 +134,10 @@ add_action('init', 'university_post_types');
 
 function university_adjust_queries($query)
 {
+	if (!is_admin() and is_post_type_archive('campus') and $query->is_main_query()) {
+		$query->set('post_per_page', -1);
+	}
+
 	if (!is_admin() and is_post_type_archive('program') and $query->is_main_query()) {
 		$query->set('orderby', 'title');
 		$query->set('order', 'ASC');
