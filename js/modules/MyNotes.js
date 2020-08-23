e0964eea10cc257bdc808a6915bf8f51cb0282ce
Max <max@example.com>
2020-08-22 22:49:23 -0700

    WP_ext Lesson 087 Edit / Update Posts with the  REST API

--- js/modules/MyNotes.js
+++ js/modules/MyNotes.js
@@ -4,20 +4,38 @@ class MyNotes {
     }
 
     events() {
-        document.querySelectorAll('.edit-note').forEach(el => el.addEventListener("click", this.editNote));
-        document.querySelectorAll('.delete-note').forEach(el => el.addEventListener("click", this.deleteNote));
+        document.querySelectorAll('.edit-note').forEach(el => el.addEventListener("click", e => this.editNote(e)));
+        document.querySelectorAll('.delete-note').forEach(el => el.addEventListener("click", e => this.deleteNote(e)));
+        document.querySelectorAll('.update-note').forEach(el => el.addEventListener("click", e => this.updateNote(e)));
     }
 
     editNote(e) {
         var thisNote = e.target.parentNode;
-        //console.log(e.target)
-        //console.log(thisNote)
-        //console.log(thisNote.querySelector('.note-title-field'))
+        if (thisNote.dataset.state == "editable") {
+            this.makeNoteReadonly(thisNote);
+        } else {
+            this.makeNoteEditable(thisNote);
+        }
+    }
+
+    makeNoteEditable(thisNote) {
+        thisNote.querySelector('.edit-note').innerHTML = '<i class="fa fa-times" aria-hidden="true"></i> Cancel';
         thisNote.querySelector('.note-title-field').removeAttribute("readonly");
         thisNote.querySelector('.note-body-field').removeAttribute("readonly");
         thisNote.querySelector('.note-title-field').classList.add('note-active-field');
         thisNote.querySelector('.note-body-field').classList.add('note-active-field');
         thisNote.querySelector('.update-note').classList.add('update-note--visible');
+        thisNote.dataset.state = "editable";
+    }
+
+    makeNoteReadonly(thisNote) {
+        thisNote.querySelector('.edit-note').innerHTML = '<i class="fa fa-pencil" aria-hidden="true"></i> Edit';
+        thisNote.querySelector('.note-title-field').setAttribute("readonly", "readonly");
+        thisNote.querySelector('.note-body-field').setAttribute("readonly", "readonly");
+        thisNote.querySelector('.note-title-field').classList.remove('note-active-field');
+        thisNote.querySelector('.note-body-field').classList.remove('note-active-field');
+        thisNote.querySelector('.update-note').classList.remove('update-note--visible');
+        thisNote.dataset.state = "cancel";
     }
 
     deleteNote(e) {
@@ -31,10 +49,35 @@ class MyNotes {
             method: 'DELETE'
         }).then(resp => {
             thisNote.classList.add('fade-out');
-            console.log("delete successsss");
+            console.log("delete success");
             console.log(resp);
         }).catch(err => {
             console.log("delete failed");
+            console.log(err)
+        });
+    }
+
+    updateNote(e) {
+        var thisNote = e.target.parentNode;
+        var url = universityData.root_url + '/wp-json/wp/v2/note/' + thisNote.getAttribute('data-id');
+        var ourUpdatedPost = {
+            title: thisNote.querySelector('.note-title-field').value,
+            content: thisNote.querySelector('.note-body-field').value,
+        }
+
+        fetch(url, {
+            headers: {
+                'X-WP-Nonce': universityData.nonce, 
+                'Content-Type': 'application/json'
+            },
+            method: 'POST', 
+            body: JSON.stringify(ourUpdatedPost)
+        }).then(resp => {
+            this.makeNoteReadonly(thisNote);
+            console.log("post success");
+            console.log(resp);
+        }).catch(err => {
+            console.log("post failed");
             console.log(err)
         });
     }
