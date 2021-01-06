9917d1a68bceaec45d5b96137d8a79f4291026aa
Max <max@example.com>
2021-01-05 19:30:02 -0800

    WP_ext Lesson 098 Note Permissions and Security (Part 2)

--- functions.php
+++ functions.php
@@ -245,6 +245,11 @@ add_filter('login_headertitle', 'ourLoginTitle');
 // Force note posts to be private 
 add_filter('wp_insert_post_data', 'makeNotePirvate');
 function makeNotePirvate($data) {
+	if ($data['post_type'] == 'note') {
+		$data['post_content'] = sanitize_textarea_field($data['post_content']);
+		$data['post_title'] = sanitize_text_field($data['post_title']);
+	}
+
 	if ($data['post_type'] == 'note' AND $data['post_status'] != 'trash') {
 		$data['post_status'] = 'private';
 	}
